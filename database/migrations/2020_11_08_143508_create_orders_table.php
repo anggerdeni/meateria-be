596881c0530057a208746042f6a5700b8fffe4be
Novian Deny Cahyo Aji <anggerdeni@gmail.com>
2020-11-09 11:16:38 +0700

    [WIP] Model and migration (#2)
    * add migration and models: user,store,product,order,order detail
    
    * delete unfinished migration and model: cart
    
    * edit readme
    
    * add migration and model cart

---
+++ database/migrations/2020_11_08_143508_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['UNPAID', 'PAID', 'PROCESSED', 'CANCELLED', 'ON DELIVERY', 'DELIVERED']);
+            $table->float('total_payment', 8, 2);
+            $table->unsignedBigInteger('buyer_id');
+            $table->unsignedBigInteger('store_id');
+            $table->timestamps();
+
+            $table->foreign('buyer_id')->references('id')->on('users');
+            $table->foreign('store_id')->references('id')->on('stores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
