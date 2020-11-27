3356672d3358c3523aada85c951aed2dc1cc315e
Novian Deny C.A <anggerdeni@gmail.com>
2020-11-27 19:41:00 +0700

    edit register request curl

--- documentation/curl_requests.sh
+++ documentation/curl_requests.sh
@@ -1,6 +1,8 @@
 curl -X POST \
   'http://127.0.0.1:40003/api/auth/register' \
   -F "email=sherlock@holmes.com"\
+  -F "phone=0812345678"\
+  -F "address=Jalan panjang"\
   -F "password=IamSHERlock"\
   -F "confirm_password=IamSHERlock"\
   -F "username=sherlock"\
